d16447197f8d64eb6ba61f3a882f26bdf8b210b7
godfreylehokoG <46106349+godfreylehokoG@users.noreply.github.com>
2021-08-03 22:07:21 +0200

    Update MrBot.py

--- MrBot.py
+++ MrBot.py
@@ -4,7 +4,7 @@ from time import sleep
 import os
 from os import environ
 
-query = ("#mars OR #spaceexploration OR #spacex")
+query = ("#ai OR #machinelearning OR #deeplearnig OR #spaceexploration OR #spacex")
 
 # Authenticate to Twitter
 CONSUMER_KEY = environ['CONSUMER_KEY']
