fc85eb9289813443a290429e1e8fccf3ccbea137
godfreylehokoG <46106349+godfreylehokoG@users.noreply.github.com>
2021-08-04 20:14:31 +0200

    Update Legacybot.py

--- Legacybot.py
+++ Legacybot.py
@@ -3,7 +3,7 @@ from time import sleep
 import os
 from os import environ
 
-query = ("#space OR #spaceexploration OR #spaceX OR #ai OR #machinelearning" OR #deeplearnig)
+query = ("#space OR #spaceexploration OR #spaceX OR #ai OR #machinelearning OR #deeplearnig")
 
 # Twitter Authentication
 CONSUMER_KEY = environ['CONSUMER_KEY']
